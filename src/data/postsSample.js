6d418a25e146fb7a4452af25a3fe51a7237f67b7
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-08 11:01:05 +0300

    adding samplePost data

---
+++ src/data/postsSample.js
@@ -0,0 +1,45 @@
+import authImage from "../assets/AOT.jpg";
+export const samplePosts = [
+  {
+    id: crypto.randomUUID(),
+    title: "About Internet!",
+    content:
+      "Are they going to release it, soon or what ? this is one of our main agenda for the week.",
+    author: "Farac Chiuno",
+    authImage: authImage,
+    reactions: {
+      thumbsUp: 0,
+      thumbsDown: 0,
+      heart: 0,
+      fire: 0,
+    },
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "About Internet!",
+    content:
+      "Are they going to release it, soon or what ? this is one of our main agenda for the week.Are they going to release it, soon or what ? this is one of our main agenda for the week.Are they going to release it, soon or what ? this is one of our main agenda for the week.",
+    author: "Farac Chiuno",
+    authImage: authImage,
+    reactions: {
+      thumbsUp: 0,
+      thumbsDown: 0,
+      heart: 0,
+      fire: 0,
+    },
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "About Internet!",
+    content:
+      "Are they going to release it, soon or what ? this is one of our main agenda for the week.Are they going to release it, soon or what ? this is one of our main agenda for the week.",
+    author: "Farac Chiuno",
+    authImage: authImage,
+    reactions: {
+      thumbsUp: 0,
+      thumbsDown: 0,
+      heart: 0,
+      fire: 0,
+    },
+  },
+];
