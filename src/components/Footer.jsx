5cffcbad58104b0dc4204abc214b009df2e485f8
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-09 07:44:53 +0300

    adding Footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className='py-4 bg-black z-50 w-full text-center text-white fixed bottom-0'>
+      <b>Solomon Muhye &copy; 2023</b>
+    </footer>
+  );
+};
+
+export default Footer;
