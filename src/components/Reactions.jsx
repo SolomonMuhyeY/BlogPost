748bd78a3a4a9d9cfb995a3bdf73ea58a2801cfa
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-09 07:48:19 +0300

    modfying Reaction section

--- src/components/Reactions.jsx
+++ src/components/Reactions.jsx
@@ -1,15 +1,47 @@
 /* eslint-disable react/prop-types */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { reactionAdded } from "../features/posts/postSlice";
+import { useDispatch } from "react-redux";
+import {
+  faFire,
+  faHeart,
+  faThumbsDown,
+  faThumbsUp,
+} from "@fortawesome/free-solid-svg-icons";
 const Reactions = ({ post }) => {
+  const dispatch = useDispatch();
   const reactionIcons = {
-    thumbsUp: "👍",
-    thumbsDown: "👎",
-    heart: "💗",
-    fire: "🔥",
+    thumbsUp: (
+      <i className='text-yellow-500'>
+        <FontAwesomeIcon icon={faThumbsUp} />
+      </i>
+    ),
+    thumbsDown: (
+      <i className='text-gray-300'>
+        <FontAwesomeIcon icon={faThumbsDown} />
+      </i>
+    ),
+    heart: (
+      <i className='text-green-500'>
+        <FontAwesomeIcon icon={faHeart} />
+      </i>
+    ),
+    fire: (
+      <i className='text-red-500'>
+        <FontAwesomeIcon icon={faFire} />
+      </i>
+    ),
   };
   const rxnBtns = Object.entries(reactionIcons).map(([name, rxn]) => {
-    console.log(post);
+    // console.log(post);
     return (
-      <button key={name} className='px-0.5 cursor-pointer'>
+      <button
+        onClick={() =>
+          dispatch(reactionAdded({ postId: post.id, reaction: name }))
+        }
+        key={name}
+        className='px-1 cursor-pointer'
+      >
         <span>{rxn}</span>
         <span>{post.reactions[name]}</span>
       </button>
