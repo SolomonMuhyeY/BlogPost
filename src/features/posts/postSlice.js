f593ee88744679dd47601be4adef5823047098bb
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-09 07:50:40 +0300

    adding new actions

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { samplePosts } from "../../data/postsSample";
 
 const initialState = {
@@ -9,10 +9,36 @@ const postSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    addNewPost: (state) => {
-      console.log(state);
+    addNewPost: {
+      reducer(state, action) {
+        state.posts.push(action.payload);
+      },
+      prepare(userId, title, content, timePosted) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            timePosted,
+            content,
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              thumbsDown: 0,
+              heart: 0,
+              fire: 0,
+            },
+          },
+        };
+      },
+    },
+    reactionAdded: (state, action) => {
+      const { postId, reaction } = action.payload;
+      const currentPost = state.posts.find((post) => post.id === postId);
+      currentPost && currentPost.reactions[reaction]++;
     },
   },
 });
-export const postReducer = postSlice.reducer;
+
+export const { reactionAdded } = postSlice.actions;
 export const selectAllPosts = (state) => state.posts;
+export const postReducer = postSlice.reducer;
