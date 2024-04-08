3cf8e2a5ab54a3e2aca8d039e820eb2fdcf93343
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-08 11:04:35 +0300

    modfying SinglePost.jsx

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,29 +1,26 @@
-import { useSelector } from "react-redux";
-import { selectAllPosts } from "../features/posts/postSlice";
+/* eslint-disable react/prop-types */
 import { useParams, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Reactions from "./Reactions";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLeftLong } from "@fortawesome/free-solid-svg-icons";
-const SinglePost = () => {
-  const { posts } = useSelector(selectAllPosts);
+const SinglePost = ({ posts }) => {
   const { id } = useParams();
   const navigate = useNavigate();
   // console.log(posts);
   const goBack = () => {
     navigate(-1);
   };
-  const currentPost = posts.find((post) => post.id === id);
-  // console.log(currentPost);
+  const currentPost = posts.find((post) => post.id === parseInt(id));
   return (
     <section
       key={currentPost.id}
-      className='bg-white border-2 my-24 p-6 md:w-3/5 m-auto'
+      className='bg-white border-2 my-24 p-6 ml-24 w-3/4 md:w-3/5 m-auto'
     >
       <div>
         <button
           onClick={goBack}
-          className='bg-red-400 py-1.5 px-2.5 duration-300 hover:scale-105'
+          className='bg-brightRed py-1.5 px-2.5 rounded duration-500 hover:scale-105'
         >
           <i className='px-1.5'>
             <FontAwesomeIcon icon={faLeftLong} />
@@ -35,7 +32,7 @@ const SinglePost = () => {
         <div className='w-32 h-32 my-2 m-auto rounded-full overflow-hidden '>
           <img
             className='h-full duration-500  hover:scale-105'
-            src={currentPost.authImage}
+            src={currentPost.image}
             alt="auhtor's image"
           />
         </div>
@@ -49,7 +46,7 @@ const SinglePost = () => {
       <div className='md:w-3/4 m-auto'>
         <p>{currentPost.content}</p>
         <Reactions post={currentPost} />
-        <Link to='#' className='text-xs underline text-blue-400'>
+        <Link to={`edit`} className='text-xs underline text-blue-400'>
           Edit Post
         </Link>
       </div>
