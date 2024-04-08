338dc5a5f846bec7b4087c0887c0141aee89088e
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-09 00:24:07 +0300

    adding new section

---
+++ src/components/GeneralBoard.jsx
@@ -0,0 +1,20 @@
+function GeneralBoard() {
+  return (
+    <aside className='w-1/2 border-2 p-4 m-4 max-h-[500px]'>
+      <h1 className='text-3xl'>General Board</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia nihil
+        ea error totam consequatur iste et beatae ut, culpa distinctio
+        voluptate. Consequuntur odit ducimus velit eos distinctio odio tempora
+        veniam?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit accusantium
+        dolor culpa vel quas, molestiae deserunt dolores nesciunt! Distinctio
+        eaque ea nam, accusantium omnis quo eos incidunt. Est, iste ipsum?
+      </p>
+    </aside>
+  );
+}
+
+export default GeneralBoard;
