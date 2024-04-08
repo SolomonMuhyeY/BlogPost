51bccaf1ad1673afaea254253eac010fb72b9690
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-09 00:23:19 +0300

    modfying the styling and adding new section

--- src/components/PostContainer.jsx
+++ src/components/PostContainer.jsx
@@ -1,13 +1,15 @@
+import GeneralBoard from "./GeneralBoard";
 import Post from "./Post";
 
 const PostContainer = () => {
   // console.log(indvPost);
   return (
-    <div className='w-4/5 m-auto pb-24'>
-      <h1 className='text-center py-6 text-4xl font-serif font-extrabold'>
-        Blog Posts
+    <div className='m-auto pb-24'>
+      <h1 className='text-center py-6 text-4xl font-serif italic font-extrabold'>
+        Poly Blog
       </h1>
-      <section>
+      <section className='flex flex-row-reverse'>
+        <GeneralBoard />
         <Post />
       </section>
     </div>
