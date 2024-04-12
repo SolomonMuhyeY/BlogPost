78ea8694939506f178e15d7be6aa1a94e1fbdab1
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-13 01:04:07 +0300

    adding backdrop filter to Header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Sidebar from "../components/Sidebar";
 export default function Home() {
   return (
     <div className='grid grid-cols-4 bg-gray-800 text-white'>
-      <header className='sticky top-0 right-0 bg-gray-800  text-center z-50  col-span-full border-b-2 border-b-slate-700'>
+      <header className='header sticky top-0 right-0 bg-gramy-800  text-center z-50  col-span-full border-b-2 border-b-slate-700'>
         <Header />
       </header>
       <main className='text-center col-span-3'>
