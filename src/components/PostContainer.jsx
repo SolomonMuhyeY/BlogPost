896e89a125c033f396183dd4718de2bc36889772
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-13 00:06:59 +0300

    modfying PostContainer.jsx

--- src/components/PostContainer.jsx
+++ src/components/PostContainer.jsx
@@ -1,15 +1,12 @@
-import GeneralBoard from "./GeneralBoard";
 import Post from "./Post";
 
 const PostContainer = () => {
-  // console.log(indvPost);
   return (
     <div className='m-auto pb-24'>
       <h1 className='text-center py-6 text-4xl font-serif italic font-extrabold'>
         Poly Blog
       </h1>
-      <section className='flex flex-row-reverse'>
-        <GeneralBoard />
+      <section>
         <Post />
       </section>
     </div>
