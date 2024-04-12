ca180c57cf5cadf432e18cc4e97d17b44243c371
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-13 00:47:13 +0300

    working on Home component ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,13 @@ import Sidebar from "../components/Sidebar";
 export default function Home() {
   return (
     <div className='grid grid-cols-4 bg-gray-800 text-white'>
-      <header className='sticky container top-0 bg-gray-800  text-center z-50  col-span-full border-b-2 border-b-slate-700'>
+      <header className='sticky top-0 right-0 bg-gray-800  text-center z-50  col-span-full border-b-2 border-b-slate-700'>
         <Header />
       </header>
       <main className='text-center col-span-3'>
         <PostContainer />
       </main>
-      <sidebar className='col-span-1 shadow-sm shadow-red-50'>
+      <sidebar className='col-span-1 shadow-sm shadow-slate-700'>
         <Sidebar />
       </sidebar>
     </div>
