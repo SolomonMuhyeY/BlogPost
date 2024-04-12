241d0d98aac7a4766cabceb20b0b4aaa641b4a8c
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-13 00:07:57 +0300

    modfying App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import PostContainer from "./components/PostContainer";
 import Layout from "./components/Layout";
 import SinglePost from "./components/SinglePost";
 import EditPost from "./components/EditPost";
+import Home from "./pages/Home";
 function App() {
   const { posts } = useSelector(selectAllPosts);
 
   return (
     <div className='App'>
-      <Routes>
+      {/* <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<PostContainer />} />
           <Route path='post/:id' element={<SinglePost posts={posts} />}></Route>
           <Route path='post/:id/edit' element={<EditPost posts={posts} />} />
         </Route>
-      </Routes>
+      </Routes> */}
+      <Home />
     </div>
   );
 }
