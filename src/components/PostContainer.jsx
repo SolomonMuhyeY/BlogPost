a880d47d6b884c9198a380afa2934665a953645f
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-13 00:49:47 +0300

    working on PostContainer component ui

--- src/components/PostContainer.jsx
+++ src/components/PostContainer.jsx
@@ -3,7 +3,7 @@ import Post from "./Post";
 const PostContainer = () => {
   return (
     <div className='m-auto pb-24'>
-      <h1 className='text-center py-6 text-4xl font-serif italic font-extrabold'>
+      <h1 className='text-center py-6 text-4xl italic font-extrabold'>
         Poly Blog
       </h1>
       <section>
