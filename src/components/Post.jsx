8a448ad82bf82e033d47fbe3c0d006859b55f9a9
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-13 01:35:40 +0300

    working on Post component ui

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,29 +2,36 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "../features/posts/postSlice";
 import Reactions from "./Reactions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faContactBook } from "@fortawesome/free-solid-svg-icons";
 const Post = () => {
   const { posts } = useSelector(selectAllPosts);
   const indvPost = posts.map((post) => {
     return (
       <div
         key={post.id}
-        className='border-2 border-slate-700 m-3 p-4 rounded-xl'
+        className='border-2 border-slate-700 px-1 py-2 rounded-xl'
       >
+        <div className='flex mx-2'>
+          <FontAwesomeIcon icon={faContactBook} />
+          <div>
+            <p className='font-extralight text-sm text-lime-400'>
+              {post.author}
+            </p>
+            <p className='text-xs italic font-extralight'>{post.department}</p>
+          </div>
+        </div>
         <div className='text-center pb-3'>
-          <div className='w-32 h-32 my-2 m-auto rounded-full overflow-hidden '>
+          <h1 className='text-center pb-3 text-2xl font-bold'>{post.title}</h1>
+          <div className='rounded-xl w-4/5 h-64 mx-auto overflow-hidden '>
             <img
-              className='h-full duration-500  hover:scale-105'
+              className='w-full object-cover h-full duration-500  hover:scale-105'
               src={post.image}
               alt="auhtor's image"
             />
           </div>
-          <p className='font-extralight text-sm text-lime-400'>{post.author}</p>
         </div>
-        <p className='text-center leading-snug pb-3 text-2xl font-bold font-mono'>
-          {post.title}
-        </p>
         <div className='text-justify px-3'>
-          <p>{post.content}</p>
           <Reactions post={post} />
           <Link
             to={`post/${post.id}`}
@@ -37,7 +44,7 @@ const Post = () => {
     );
   });
   return (
-    <div className='grid md:grid-cols-2 gap-8 mx-5 justify-center'>
+    <div className='grid md:grid-cols-2 gap-8 mx-12 justify-center'>
       {indvPost}
     </div>
   );
