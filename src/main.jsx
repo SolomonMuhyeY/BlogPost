90250c5ef83788fa514f9f2aaa68d15ce9df509c
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-13 00:47:59 +0300

    moving store to App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./redux/store.js";
+import { store } from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <Router>
+      <React.StrictMode>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </React.StrictMode>
+    </Router>
   </Provider>
 );
