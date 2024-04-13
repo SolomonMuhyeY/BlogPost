f5f2f680ca86de23051d569b084c428d6501aee4
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-14 00:23:24 +0300

    adding Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./features/posts/postSlice";
-import PostContainer from "./components/PostContainer";
 import Layout from "./components/Layout";
 import SinglePost from "./components/SinglePost";
 import EditPost from "./components/EditPost";
-import Home from "./pages/Home";
+import PostContainer from "./components/PostContainer";
 function App() {
   const { posts } = useSelector(selectAllPosts);
 
   return (
     <div className='App'>
-      {/* <Routes>
+      <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<PostContainer />} />
           <Route path='post/:id' element={<SinglePost posts={posts} />}></Route>
           <Route path='post/:id/edit' element={<EditPost posts={posts} />} />
         </Route>
-      </Routes> */}
-      <Home />
+      </Routes>
+      {/* <Home /> */}
     </div>
   );
 }
