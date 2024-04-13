92b59395916c00b3c8903efb6d7b67c4a38bd045
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-14 00:24:33 +0300

    working on Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import Footer from "./Footer";
+import Header from "./Header";
 
 const Layout = () => {
   return (
-    <div>
-      <Sidebar />
-      <Outlet />
-      <Footer />
+    <div className='grid grid-cols-4 bg-gray-800 text-white'>
+      <header className='header sticky top-0 right-0 bg-gramy-800  text-center z-50  col-span-full border-b-2 border-b-slate-700'>
+        <Header />
+      </header>
+      <main className='text-center  col-span-3'>
+        <Outlet />
+      </main>
+      <sidebar className='col-span-1 shadow-sm shadow-slate-700'>
+        <Sidebar />
+      </sidebar>
     </div>
   );
 };
