7a0933e5cdd1afb904dc8a18a6aa0e4ec4128609
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-13 17:27:22 +0300

    modfying Post ui

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,26 +3,32 @@ import { useSelector } from "react-redux";
 import { selectAllPosts } from "../features/posts/postSlice";
 import Reactions from "./Reactions";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faContactBook } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircleDot,
+  faCircleUser,
+  faContactBook,
+} from "@fortawesome/free-solid-svg-icons";
 const Post = () => {
   const { posts } = useSelector(selectAllPosts);
   const indvPost = posts.map((post) => {
     return (
       <div
         key={post.id}
-        className='border-2 border-slate-700 px-1 py-2 rounded-xl'
+        className='border-2 border-slate-700 px-1 py-3 rounded-xl'
       >
         <div className='flex mx-2'>
-          <FontAwesomeIcon icon={faContactBook} />
-          <div>
-            <p className='font-extralight text-sm text-lime-400'>
+          <FontAwesomeIcon icon={faCircleUser} />
+          <div className='mx-2'>
+            <p className='font-extralight text-sm text-justify text-lime-400'>
               {post.author}
             </p>
             <p className='text-xs italic font-extralight'>{post.department}</p>
           </div>
         </div>
         <div className='text-center pb-3'>
-          <h1 className='text-center pb-3 text-2xl font-bold'>{post.title}</h1>
+          <h1 className='w-4/5 mx-auto py-5 text-xl italic font-bold'>
+            {post.title}
+          </h1>
           <div className='rounded-xl w-4/5 h-64 mx-auto overflow-hidden '>
             <img
               className='w-full object-cover h-full duration-500  hover:scale-105'
