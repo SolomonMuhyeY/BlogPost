fed970c681becf91af727d0cf636a32524d8bdb1
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-14 00:47:07 +0300

    adding Link to the sidebars

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,41 +6,42 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../styles/component-styling.css";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <>
       <div className='item-container fixed w-full'>
-        <div className='item flex border-b-2 border-slate-700'>
+        <Link className='item flex border-b-2 border-slate-700'>
           <i className='bg-transparent px-5'>
             <FontAwesomeIcon icon={faBloggerB} />
           </i>
           <div className='pb-2'>Blogs</div>
-        </div>
-        <div className='item flex'>
+        </Link>
+        <Link className='item flex'>
           <i className='bg-transparent px-5'>
             <FontAwesomeIcon icon={faSearch} />
           </i>
           <div>Search</div>
-        </div>
-        <div className='item flex'>
+        </Link>
+        <Link to='about' className='item flex'>
           <i className='bg-transparent px-5'>
             <FontAwesomeIcon icon={faChalkboardUser} />
           </i>
           <div>About</div>
-        </div>
-        <div className='item flex'>
+        </Link>
+        <Link className='item flex'>
           <i className='bg-transparent px-5'>
             <FontAwesomeIcon icon={faThinkPeaks} />
           </i>
           <div>Insights</div>
-        </div>
-        <div className='item flex'>
+        </Link>
+        <Link className='item flex'>
           <i className='bg-transparent px-5'>
             <FontAwesomeIcon icon={faGear} />
           </i>
           <div>Settings</div>
-        </div>
+        </Link>
       </div>
     </>
   );
