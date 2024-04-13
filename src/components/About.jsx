5047db444aa91f1dbcc0cb038372b9b1d72ef147
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-14 00:47:49 +0300

    adding About section

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+        iusto! Nam, voluptas veritatis. Architecto nostrum a ducimus nam ut vel
+        atque quibusdam at, temporibus accusantium debitis ex eum modi facere
+        iusto odio fuga nulla unde aliquid consequatur necessitatibus esse
+        recusandae sequi! Aspernatur voluptate incidunt obcaecati.
+      </p>
+    </div>
+  );
+}
